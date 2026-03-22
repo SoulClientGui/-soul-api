545ce6bb495e6012c7ce63530072365cf405e40f
SoulClientGui <tiktoklive74000@gmail.com>
2026-03-22 21:49:49 +0000

    Update SoulV1.lua

--- SoulV1.lua
+++ SoulV1.lua
@@ -1,5 +1,5 @@
 -- Soul V1 | by Soul
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/SoulClientGui/-soul-api/main/SoulV1.lua"))()
+-- loadstring(game:HttpGet("https://github.com/SoulClientGui/-soul-api/edit/main/SoulV1.lua"))()
 
 local Players          = game:GetService("Players")
 local _gui = Players.LocalPlayer:WaitForChild("PlayerGui")
